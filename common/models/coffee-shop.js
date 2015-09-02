fc86cb3e0afef2bd9f38e68ff8097a90b92e7227
Josh Bedo <joshbedo@Joshs-MacBook-Pro.local>
2015-09-02 10:43:08 -0400

    Setting up project

---
+++ common/models/coffee-shop.js
@@ -0,0 +1,28 @@
+module.exports = function(CoffeeShop) {
+  CoffeeShop.status = function(cb) {
+    var currentDate = new Date();
+    var currentHour = currentDate.getHours();
+    var OPEN_HOUR = 6;
+    var CLOSE_HOUR = 20;
+
+    console.log('Current hour is ' + currentHour);
+
+    var resp;
+
+    if (currentHour > OPEN_HOUR && currentHour < CLOSE_HOUR) {
+      response = 'We are open for business.';
+    } else {
+      response = 'Sorry, we are closed. Open faily from 6am to 8pm.';
+    }
+
+    cb(null, resp);
+  };
+
+  CoffeeShop.remoteMethod(
+    'status',
+    {
+      http: { path: '/status', verb: 'get' },
+      returns: { arg: 'status', type: 'string' }
+    }
+  );
+};
